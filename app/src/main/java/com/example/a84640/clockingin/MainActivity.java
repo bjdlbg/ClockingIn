84ffadb4acc3109000df2091368a6ca913206456
Elichen <1312664470@qq.com>
2019-03-01 12:58:34 +0800

    添加选择按钮

--- app/src/main/java/com/example/a84640/clockingin/MainActivity.java
+++ app/src/main/java/com/example/a84640/clockingin/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.a84640.clockingin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * 软件主程序入口
@@ -10,10 +13,23 @@ import android.os.Bundle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Button myButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        myButton=(Button)findViewById(R.id.button1);
+        //
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                Toast.makeText(getApplication(),"hahah",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
